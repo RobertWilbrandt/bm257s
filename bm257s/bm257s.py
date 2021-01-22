91300b43b48b616c378b175e402e4bd037dca9ff
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-01-22 22:30:37 +0100

    Open serial connection to multimeter

--- bm257s/bm257s.py
+++ bm257s/bm257s.py
@@ -1,3 +1,30 @@
+"""Serial interface library for brymen bm257s multimeters"""
+import serial
+
+
 class BM257sSerialInterface:
-    def __init__(self):
-        print("abc")
+    """Serial interface used to communicate with brymen bm257s multimeters
+
+    :param port: Device name to use
+    :type port: str
+    """
+
+    def __init__(self, port="/dev/ttyUSB0"):
+        self._serial = serial.Serial(
+            port,
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            bytesize=serial.EIGHTBITS,
+            stopbits=serial.STOPBITS_ONE,
+        )
+
+    def close(self):
+        """Closes the used serial port
+        """
+        self._serial.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.close()
