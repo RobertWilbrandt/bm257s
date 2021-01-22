94f05946477fe044152aedddbc072790b27b32db
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-01-22 22:48:28 +0100

    Read data from serial port

--- bm257s/bm257s.py
+++ bm257s/bm257s.py
@@ -2,6 +2,18 @@
 import serial
 
 
+def decode_lcd(data):
+    """Decodes the serial data sent from the multimeter
+
+    :param data: 15 byte data package from multimeter
+    :type data: list
+
+    :return: Decoded LCD state
+    :rtype: dict
+    """
+    return None
+
+
 class BM257sSerialInterface:
     """Serial interface used to communicate with brymen bm257s multimeters
 
@@ -17,6 +29,15 @@ class BM257sSerialInterface:
             bytesize=serial.EIGHTBITS,
             stopbits=serial.STOPBITS_ONE,
         )
+
+    def read_lcd(self):
+        """Reads the current state of the LCD
+
+        :return: Decoded LCD state
+        :rtype: dict
+        """
+        msg = self._serial.read(15)
+        print(msg)
 
     def close(self):
         """Closes the used serial port
