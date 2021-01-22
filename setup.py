23693c08488c5f5137bda14b9ca222b7ee0e088d
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-01-22 21:10:26 +0100

    Add setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""Setup of brymen bm257s serial interface library"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="bm257s",
+    version="0.1",
+    author="Robert Wilbrandt",
+    author_email="robert@stamm-wilbrandt.de",
+    description="Small python library to interface with brymen bm257s multimeters",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
